ea11282607caf74a74e98259bda32c1d6f4b2590
TheFictionRed <143562833+TheFictionRed@users.noreply.github.com>
2024-02-15 17:59:03 -0600

    Add files via upload

---
+++ EVA1_5_CLASE_PERSONA/src/eva1_5_clase_persona/EVA1_5_CLASE_PERSONA.java
@@ -0,0 +1,26 @@
+
+package eva1_5_clase_persona;
+
+public class EVA1_5_CLASE_PERSONA {
+
+    public static void main(String[] args) {
+       
+        Persona p1 = new Persona();
+        
+        
+        
+        
+    }
+    
+}
+class Persona{
+    //LOS ATRIBUTOS NO DEBEN SER ACCESIBLE DIRECTAMENTE
+    
+private String NOMBRE;
+private String APELLIDOS;
+private int EDAD;
+    
+    //LOS METODOS QUE SEAN PUBLICOS SON LA INTERFAZ Y SON PARA MANIPULAR LOS ATRIBUTOS EN JAVA HAY UN ESTANDAR 
+    //METODOS SET Y GET
+
+}
